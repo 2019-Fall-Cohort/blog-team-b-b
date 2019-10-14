2a19587eb9d007fda4b8ca5b3d3bcf79989cc341
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-14 16:29:46 -0400

    filled out BlogPost class

--- src/main/java/org/wcci/blog/BlogPost.java
+++ src/main/java/org/wcci/blog/BlogPost.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class BlogPost {
@@ -14,24 +15,50 @@ public class BlogPost {
 	private String postTitle;
 	private PostAuthor postAuthor;
 	private PostGenre postGenre;
-	private LocalDate reviewDate= LocalDate.now(); 
+	private LocalDate reviewDate = LocalDate.now(); 
 	
-	public BlogPost() {
-		
+	public BlogPost(String title, PostAuthor author, PostGenre genre, LocalDate date) {
+		this.postTitle = title;
+		this.postAuthor = author;
+		this.postGenre = genre;
+		this.reviewDate = date;
 	}
 	
+	public BlogPost() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public String getPostTitle() {
+		return postTitle;
+	}
+
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+
+	public PostAuthor getPostAuthor() {
+		return postAuthor;
+	}
+
+	public void setPostAuthor(PostAuthor postAuthor) {
+		this.postAuthor = postAuthor;
+	}
+
+	public PostGenre getPostGenre() {
+		return postGenre;
+	}
+//
+//	public void setPostGenre(PostGenre postGenre) {
+//		this.postGenre = postGenre;
+//	}
+
+	public LocalDate getReviewDate() {
+		return reviewDate;
+	}
+
 	
 	
 }
