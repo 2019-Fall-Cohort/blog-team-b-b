2f46ce74f62e6f0721f5607a5e7408554e7fb079
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-16 16:25:28 -0400

    added folder hierarchy for templates css images and prettied up index page

--- src/main/java/org/wcci/blog/GenreStorage.java
+++ src/main/java/org/wcci/blog/GenreStorage.java
@@ -1,7 +1,9 @@
 package org.wcci.blog;
 
+import org.hibernate.boot.archive.scan.spi.ClassDescriptor.Categorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.wcci.blog.Genre;
 
 @Repository
 public class GenreStorage {
@@ -12,4 +14,13 @@ public class GenreStorage {
 	public Genre addGenre(Genre genreToAdd) {
 		return genreRepo.save(genreToAdd);
 	}
+	
+	public Genre findGenre(Long id) {
+		return genreRepo.findById(id).get();
+	}
+	
+	public Iterable<Genre> findAllTheGenres(){
+		return genreRepo.findAll();
+	}
+	
 }
