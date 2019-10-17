aa07621c30f674bf66a83b557be0960f39df97b7
BarbaraTaylor <btaylor1787@gmail.com>
2019-10-17 01:38:54 -0400

    main classes and controllers

---
+++ src/main/java/org/wcci/blog/Post.java
@@ -0,0 +1,69 @@
+package org.wcci.blog;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Post {
+	@Id
+	@GeneratedValue
+	private Long id; 
+	
+	private String postTitle;
+	private String postBody;
+	@ManyToOne
+	private Author author;
+	private LocalDate reviewDate = LocalDate.now(); 
+	@ManyToOne
+	private Genre genre;
+	@ManyToMany
+	private List<Tag> tags;
+	
+	public Post() {
+	}
+	
+	public Post(String givenTitle, String givenPostBody, Author givenAuthor, 
+			LocalDate givenDate, Genre givenGenre) {
+		this.postTitle = givenTitle;
+		this.postBody = givenPostBody;
+		this.author = givenAuthor;
+		this.reviewDate = givenDate;
+		this.genre = givenGenre;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getPostTitle() {
+		return postTitle;
+	}
+
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public LocalDate getReviewDate() {
+		return reviewDate;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+	
+	public List<Tag> getTags() {
+		return tags;
+	}
+}
---
+++ src/main/java/org/wcci/blog/TagStorage.java
@@ -0,0 +1,26 @@
+package org.wcci.blog;
+
+import org.hibernate.boot.archive.scan.spi.ClassDescriptor.Categorization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.wcci.blog.Tag;
+
+@Repository
+public class TagStorage {
+
+	@Autowired
+	TagRepository tagRepo;
+
+	public Tag addTag(Tag tagToAdd) {
+		return tagRepo.save(tagToAdd);
+	}
+	
+	public Tag findTag(Long id) {
+		return tagRepo.findById(id).get();
+	}
+	
+	public Iterable<Tag> findAllTheTags(){
+		return tagRepo.findAll();
+	}
+	
+}
